4738d687cddfb2e5485d2881a1aa97511532c403
arti47 <72919665+arti47@users.noreply.github.com>
2026-07-12 22:49:14 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,14 +7,13 @@
 // Anonymous auth), paste your web app config below, set FIREBASE_ENABLED = true,
 // and deploy database.rules.json. NEVER commit real keys to a public repo.
 
-export const FIREBASE_ENABLED = false;
+export const FIREBASE_ENABLED = true;
 
 export const firebaseConfig = {
-  apiKey:            'PASTE_API_KEY',
-  authDomain:        'PASTE_PROJECT.firebaseapp.com',
-  databaseURL:       'https://PASTE_PROJECT-default-rtdb.firebaseio.com',
-  projectId:         'PASTE_PROJECT',
-  storageBucket:     'PASTE_PROJECT.appspot.com',
-  messagingSenderId: 'PASTE_SENDER_ID',
-  appId:             'PASTE_APP_ID',
+  apiKey: "AIzaSyBciI9cH19KrD4ntKgbZsvembbwAubM2Ko",
+  authDomain: "imperium-player.firebaseapp.com",
+  projectId: "imperium-player",
+  storageBucket: "imperium-player.firebasestorage.app",
+  messagingSenderId: "560274847711",
+  appId: "1:560274847711:web:5450d7bb47f2cce8c6366a"
 };
